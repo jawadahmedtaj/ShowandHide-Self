5a469af9fffa63cfde7b3323e13eaed960e1600b
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-03-26 09:31:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/showandhide/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.showandhide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showElemnt(View view){
+
+    }
+    public void hideElement(View view){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
