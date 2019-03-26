3eac8f0d5aa60cce9ed8bbb0c7558c1896220eab
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-03-26 09:55:24 +0100

    completed

--- app/src/main/java/com/example/showandhide/MainActivity.java
+++ app/src/main/java/com/example/showandhide/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.showandhide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
-    public void showElemnt(View view){
-
+    public void showElement(View view){
+        textView.setVisibility(View.VISIBLE);
     }
     public void hideElement(View view){
-
+        textView.setVisibility(View.INVISIBLE);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textElement);
     }
 }
